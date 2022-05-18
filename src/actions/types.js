eb365409c73a6436ca4bdc0f99d9005e3e35ce57
Izuchukwu Onukwube <57435925+izuchukwu-eric@users.noreply.github.com>
2022-05-19 00:22:14 +0100

    setup redux

---
+++ src/actions/types.js
@@ -0,0 +1,2 @@
+export const GET_REPONSE = "GET_RESPONSE";
+export const GET_RESPONSE_FAIL = "GET_RESPONSE_FAIL";
---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from "redux";
+import request from "./request"
+export default combineReducers({
+  request
+});
